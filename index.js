0c21089fc9a353d42feb6dac5403194638c6db35
unknown <adetunmbikenny@gmail.com>
2022-11-04 19:57:00 +0100

    cors issue

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ const cors = require("cors");
 const app = express();
 
 const port = process.env.PORT || 5000;
-
-const includesOperation = (str) => {
-  if (str.includes("add")) {
-    return "";
-  }
-};
-
+app.use(cors());
 app.use(express.json());
 
 app.post("/", (req, res) => {
   let result = null;
   let x = Number(req.body.x);
   let y = Number(req.body.y);
-  console.log(req.body.x, typeof x);
+  console.log(req.body);
   /** 
   @type {string} operationType
  */
@@ -41,7 +35,11 @@ app.post("/", (req, res) => {
   //     result = x / y;
   //   }
 
-  console.log(result);
+  console.log({
+    slackUserName: "aaronkenny",
+    result,
+    operation_type,
+  });
   res.json({
     slackUserName: "aaronkenny",
     result,
