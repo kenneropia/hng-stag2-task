89271da61729d6f2cebb3b1181fc86127fefc639
unknown <adetunmbikenny@gmail.com>
2022-11-04 19:52:55 +0100

    cors issue

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-const includesOperation = (str) => {
-  if (str.includes("add")) {
-    return "";
-  }
-};
-
+app.use(cors());
 app.use(express.json());
 
 app.post("/", (req, res) => {
   let result = null;
   let x = Number(req.body.x);
   let y = Number(req.body.y);
-  console.log(req.body.x, typeof x);
-  /** 
-  @type {string} operationType
- */
+  console.log(req.body);
   let operation_type = null;
-  let operationType = req.body.operation_type;
+  let operationType = req.body.operation_type.toLowerCase();
   if (operationType.includes("add")) {
     operation_type = "addition";
     result = x + y;
@@ -36,11 +29,20 @@ app.post("/", (req, res) => {
     result = x * y;
   }
 
+  if (operationType.includes("pro")) {
+    operation_type = "product";
+    result = x * y;
+  }
+
   //   if (operationType.includes("div")) {
   //     result = x / y;
   //   }
 
-  console.log(result);
+  console.log({
+    slackUserName: "aaronkenny",
+    result,
+    operation_type,
+  });
   res.json({
     slackUserName: "aaronkenny",
     result,
